61cad42efc0bebe763f7702896aabdc5bf4bfffe
Admin <nirun.tk@gmail.com>
2025-02-26 21:14:58 +0700

    first update

---
+++ app/src/main/java/com/example/shop2/Activity/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.shop2.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shop2.databinding.ActivitySplashBinding
+
+class splashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+}
